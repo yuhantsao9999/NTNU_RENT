d2314029c90a3072cc178f4cdb3fe9bb9c51133a
boyuchen0224 <boyuchen0224@gmail.com>
2020-12-24 17:24:19 +0800

    Add all rent and sell

--- routes/image.js
+++ routes/image.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const router = express.Router()
-const { getUserRent, getUserSell } = require('../module/image')
+const { getUserRent, getUserSell, getAllSell, getAllRent } = require('../module/image')
+
+router.get('/rent_image_all', async (req, res) => {
+  const result = await getAllRent()
+  if (result.error) {
+    res.status(404).send('Image get error.')
+  }
+  res.send(result.data)
+})
+
+router.get('/sell_image_all', async (req, res) => {
+  const result = await getAllSell()
+  if (result.error) {
+    res.status(404).send('Image get error.')
+  }
+  res.send(result.data)
+})
 
 router.get('/rent_image', async (req, res) => {
   const email = req.query.email
