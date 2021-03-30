592edd31e9c8343160a0d338e70f2092c9f90b06
yuhantsao9999 <yuhantsao9999@gmail.com>
2021-03-30 19:39:19 +0800

    delete:delete node_module

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
 
-app.use(bodyParser.json({ limit: '50000mb' }))
-app.use(bodyParser.urlencoded({ limit: '50000mb', extended: true }))
+app.use(bodyParser.json({ limit: '50000mb' }));
+app.use(bodyParser.urlencoded({ limit: '50000mb', extended: true }));
 
-app.use(express.static(path.join(__dirname, '/public'), { index: 'index.html' }))
-app.use(express.static(__dirname, { dotfiles: 'allow' }))
+app.use(express.static(path.join(__dirname, '/public'), { index: 'index.html' }));
+app.use(express.static(__dirname, { dotfiles: 'allow' }));
 
-const image = require('./routes/image')
-app.use('/', image)
+// const image = require('./routes/image');
+// app.use('/', image);
 
-const account = require('./routes/account')
-app.use('/', account)
+// const account = require('./routes/account');
+// app.use('/', account);
 
-const upload = require('./routes/upload')
-app.use('/', upload)
+// const upload = require('./routes/upload');
+// app.use('/', upload);
 
 app.get('/', (req, res) => {
-  res.send('ok')
-})
+    res.send('ok');
+});
 
-app.listen((3000), () => console.log('伺服器已經啟動在http://localhost:3000/'))
+app.listen(3000, () => console.log('伺服器已經啟動在http://localhost:3000/'));
--- model/db.js
+++ model/db.js
@@ -1,32 +1,38 @@
-const mysql = require('mysql2')
-require('dotenv').config()
+const mysql = require('mysql2');
+require('dotenv').config();
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } = process.env
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } = process.env;
 
 const pool = mysql.createPool({
-  connectionLimit: 100,
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_DATABASE,
-  port: DB_PORT,
-})
+    connectionLimit: 100,
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    port: DB_PORT,
+});
 
 const query = (sql, params) => {
-  return new Promise((resolve, reject) => {
-    pool.getConnection((err, connection) => {
-      if (err) reject(new Error(err))
-      connection.query(sql, params, (err, result) => {
-        connection.release()
-        if (err) reject(new Error(err))
-        resolve(result)
-      })
-    })
-  })
-}
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            if (err) reject(new Error(err));
+            connection.query(sql, params, (err, result) => {
+                connection.release();
+                if (err) reject(new Error(err));
+                resolve(result);
+            });
+        });
+    });
+};
+pool.getConnection((error, connection) => {
+    console.log('MySQL connected');
+    if (error) {
+        if (connection) connection.release();
+        return;
+    }
+});
 
 module.exports = {
-  pool: pool,
-  query: query,
-
-}
+    pool: pool,
+    query: query,
+};
