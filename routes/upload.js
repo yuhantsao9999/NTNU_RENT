99ec6e7901f9fa8fe679f36f75227cda8de2e033
yuhantsao9999 <yuhantsao9999@gmail.com>
2021-06-16 20:47:45 +0800

    feat:建立 s3 連線以及聯絡方式

--- routes/upload.js
+++ routes/upload.js
@@ -1,31 +1,36 @@
-const express = require('express')
-const router = express.Router()
-const { multerS3 } = require('../utils/multerS3')
-const { rentList } = require('../controller/rentList')
-const { sellList } = require('../controller/sellList')
-const imageCheck = require('../middlewares/imageCheck')
-const rentCheck = require('../middlewares/rentCheck')
+const express = require('express');
+const router = express.Router();
+const { multerS3 } = require('../utils/multerS3');
+const { rentList } = require('../controller/rentList');
+const { sellList } = require('../controller/sellList');
+const imageCheck = require('../middlewares/imageCheck');
+const rentCheck = require('../middlewares/rentCheck');
 
-router.post('/rent_upload', [multerS3.array('image'),
-  imageCheck,
-  rentCheck,
-  async (req, res) => {
-    const result = await rentList(req.files, req.body.data)
-    if (result.error) {
-      res.status(404).send('Error on mysql.')
-    }
-    res.send(result.data)
-  }])
+router.post('/rent_upload', [
+    multerS3.array('image'),
+    imageCheck,
+    rentCheck,
+    async (req, res) => {
+        console.log('enter /rent_upload');
+        const result = await rentList(req.files, req.body.data);
+        if (result.error) {
+            res.status(404).send('Error on mysql.');
+        }
+        res.send(result.data);
+    },
+]);
 
-router.post('/sell_upload', [multerS3.array('image'),
-  imageCheck,
-  rentCheck,
-  async (req, res) => {
-    const result = await sellList(req.files, req.body.data)
-    if (result.error) {
-      res.status(404).send('Error on mysql.')
-    }
-    res.send(result.data)
-  }])
+router.post('/sell_upload', [
+    multerS3.array('image'),
+    imageCheck,
+    rentCheck,
+    async (req, res) => {
+        const result = await sellList(req.files, req.body.data);
+        if (result.error) {
+            res.status(404).send('Error on mysql.');
+        }
+        res.send(result.data);
+    },
+]);
 
-module.exports = router
+module.exports = router;
