f9571320f2524e70628df3ce5a0bd4513e2250ed
Allen5123 <allen23648858@yahoo.com>
2021-06-17 01:18:48 +0800

    contract rows object divide into basis&details

--- public/js/admin_contract.js
+++ public/js/admin_contract.js
@@ -2,10 +2,10 @@ let contract = new Vue({
     el : '#contract',
     data : {
         rows : [
-            // {contract_id:121312, publish_id:02, rent_id:03, c_status:'continue', start_date:'2021-06-06', end_date:'2021-07-01', delDisplay:false},
-            // {contract_id:999999, publish_id:07, rent_id:05, c_status:'continue', start_date:'2021-05-06', end_date:'2021-06-01', delDisplay:false},
-            // {contract_id:777777, publish_id:17, rent_id:25, c_status:'finish', start_date:'2021-03-05', end_date:'2021-08-31', delDisplay:false},
-            // {contract_id:663636, publish_id:03, rent_id:12, c_status:'finish', start_date:'2021-04-07', end_date:'2021-07-03', delDisplay:false}
+            // {contract_id:121312, publish_id:02, rent_id:03, c_status:'continue', start_date:null, end_date:null, delDisplay:false},
+            // {contract_id:999999, publish_id:07, rent_id:05, c_status:'continue', start_date:null, end_date:null, delDisplay:false},
+            // {contract_id:777777, publish_id:17, rent_id:25, c_status:'finish', start_date:null, end_date:null, delDisplay:false},
+            // {contract_id:663636, publish_id:03, rent_id:12, c_status:'finish', start_date:null, end_date:null, delDisplay:false}
         ],
         thFields : [
             {name:'contract_id', text:'契約編號'}, 
@@ -20,11 +20,19 @@ let contract = new Vue({
         /* fetch from db*/
         try {
             const result = await fetch('/admin/contract').then((res) => {return res.json();});
-            console.log(result);
-            result['data'].forEach(element => {
-                element['delDisplay'] = false;
-                this.rows.push(element);
-            });
+            if (result.status === 'ok') {
+                result['data'].forEach(dbdata => {
+                    let data = {outline:{}, 
+                        details:{start_date:null, end_date:null, publish_eval:null, rent_eval:null}, 
+                        delDisplay:false};
+                    this.thFields.forEach((field) => {
+                        data['outline'][field.name] = dbdata[field.name];
+                    })
+                    this.rows.push(data);
+                });
+                console.log(this.rows);
+            }
+            else {throw 'Fetch error';}
         }
         catch (err) {
             console.log(err);
@@ -32,11 +40,36 @@ let contract = new Vue({
         this.SortTable(null, 'c_status');
     },
     methods : {
-        Details : function (event, row) {
+        Details : async function (event, row) {
             // console.log('Details', idx);
+            if (row['details']['start_date'] === null) {
+                try {
+                    const result = await fetch('/admin/contract/details', {
+                        method:'POST',
+                        headers : {
+                            'content-type' :　'application/json'
+                        },
+                        body : JSON.stringify({
+                            contract_id : row['outline']['contract_id']
+                        })
+                    }).then((res) => {return res.json();});
+                    console.log(result);
+                    if (result.status === 'ok') {
+                        row['details']['start_date'] = result['data'][0]['start_date'];
+                        row['details']['end_date'] = result['data'][0]['end_date'];
+                        row['details']['publish_eval'] = result['data'][0]['publish_eval'];
+                        row['details']['rent_eval'] = result['data'][0]['rent_eval'];
+                    }
+                    else {throw 'Fetch error';}
+                }
+                catch (err) {
+                    console.log(err);
+                }
+            }
             if (event.target.tagName === 'TD') {
                 row.delDisplay = !row.delDisplay;
             }
+            console.log(this.rows);
         },
         MarkContract : function (event, idx) {
             // console.log('DeleteContract', row);
@@ -50,8 +83,8 @@ let contract = new Vue({
                 }
                 else {
                     this.rows.sort((a, b) => {
-                        if (a[field] > b[field]) {return 1;}
-                        else if (a[field] < b[field]) {return -1;}
+                        if (a['outline'][field] > b['outline'][field]) {return 1;}
+                        else if (a['outline'][field] < b['outline'][field]) {return -1;}
                         else {return 0;}
                     });
                     this.lastSort = field;
@@ -62,10 +95,9 @@ let contract = new Vue({
     watch : {
         "toDelete.action" : function () {
             if (this.toDelete.action) {
-                this.rows[this.toDelete.targetidx].c_status = 'termination';
+                this.rows[this.toDelete.targetidx]['outline']['c_status'] = 'termination';
             }
-            this.toDelete.targetidx = null;
-            this.toDelete.action = null;
+            this.toDelete.targetidx = this.toDelete.action = null;
         }
     }
 });
--- routes/admin_route.js
+++ routes/admin_route.js
@@ -3,21 +3,28 @@ const router = express.Router();
 const mysql = require('../model/db');
 router.get('/contract', async (req, res) => {
     try {
-        const qryStr = 'SELECT contract_id, publish_id, rent_id, c_status, start_date, end_date from Contract';
+        const qryStr = 'SELECT contract_id, publish_id, rent_id, c_status FROM Contract';
         const data = await mysql.query(qryStr, []);
-        console.log(data);
+        // console.log(data);
         return res.json({status:'ok', data:data});
     }
     catch (err) {
         console.log(err);
         return res.json({status:'error', data:null});
     }
-    res.json({status:'contract', 
-    data:[{contract_id:121312, publish_id:02, rent_id:03, c_status:'continue', start_date:'2021-06-06', end_date:'2021-07-01'},
-    {contract_id:999999, publish_id:07, rent_id:05, c_status:'continue', start_date:'2021-05-06', end_date:'2021-06-01'},
-    {contract_id:777777, publish_id:17, rent_id:25, c_status:'finish', start_date:'2021-03-05', end_date:'2021-08-31'},
-    {contract_id:663636, publish_id:03, rent_id:12, c_status:'finish', start_date:'2021-04-07', end_date:'2021-07-03'}
-    ]});
+});
+router.post('/contract/details', async (req, res) => {
+    const contract_id = req.body.contract_id;
+    try {
+        const qryStr = 'SELECT start_date, end_date, publish_eval, rent_eval FROM Contract WHERE contract_id = ?';
+        const data = await mysql.query(qryStr, [contract_id.toString()]);
+        // console.log(data);
+        return res.json({status:'ok', data:data});
+    }
+    catch (err) {
+        console.log(err);
+        return res.json({status:'error', data:null});
+    }
 });
 router.get('/account', (req, res) => {
     res.json({status:'contract'});
