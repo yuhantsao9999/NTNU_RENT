68ca203b4092e5ac314e508b4d58021ed2b5e70a
Allen5123 <allen23648858@yahoo.com>
2021-06-17 18:55:45 +0800

    add admin_contact filter feature

--- public/js/admin_contract.js
+++ public/js/admin_contract.js
@@ -1,12 +1,7 @@
 let contract = new Vue({
     el : '#contract',
     data : {
-        rows : [
-            // {contract_id:121312, publish_id:02, rent_id:03, c_status:'continue', start_date:null, end_date:null, delDisplay:false},
-            // {contract_id:999999, publish_id:07, rent_id:05, c_status:'continue', start_date:null, end_date:null, delDisplay:false},
-            // {contract_id:777777, publish_id:17, rent_id:25, c_status:'finish', start_date:null, end_date:null, delDisplay:false},
-            // {contract_id:663636, publish_id:03, rent_id:12, c_status:'finish', start_date:null, end_date:null, delDisplay:false}
-        ],
+        rows : [],
         thFields : [
             {name:'contract_id', text:'契約編號'}, 
             {name:'publish_id', text:'出租方'},
@@ -14,32 +9,42 @@ let contract = new Vue({
             {name:'c_status', text:'契約狀態'}
         ],
         lastSort : '',
-        toDelete : {targetidx:null, action:null}
+        toTerminate : {targetidx:null, action:null, targetcontract_id:null},
+        Filter : {contract_id:'', publish_id:'', rent_id:'', c_status:'none'}
     },
     created : async function () {
-        /* fetch from db*/
         try {
-            const result = await fetch('/admin/contract').then((res) => {return res.json();});
-            if (result.status === 'ok') {
-                result['data'].forEach(dbdata => {
-                    let data = {outline:{}, 
-                        details:{start_date:null, end_date:null, publish_eval:null, rent_eval:null}, 
-                        delDisplay:false};
-                    this.thFields.forEach((field) => {
-                        data['outline'][field.name] = dbdata[field.name];
-                    })
-                    this.rows.push(data);
-                });
-                console.log(this.rows);
-            }
-            else {throw 'Fetch error';}
+            await this.FetchOutline();
+            this.SortTable(null, 'c_status');
         }
         catch (err) {
             console.log(err);
         }
-        this.SortTable(null, 'c_status');
     },
     methods : {
+        FetchOutline : async function () {
+            try {
+                const result = await fetch('/admin/contract').then((res) => {return res.json();});
+                if (result.status === 'ok') {
+                    result['data'].forEach(dbdata => {
+                        let data = {
+                            outline:{}, 
+                            details:{start_date:null, end_date:null, publish_eval:null, rent_eval:null},
+                            display:{unit:true, details:false}
+                        };
+                        this.thFields.forEach((field) => {
+                            data['outline'][field.name] = dbdata[field.name];
+                        })
+                        this.rows.push(data);
+                    });
+                    // console.log(this.rows);
+                }
+                else {throw 'Fetch error';}
+            }
+            catch (err) {
+                throw err;
+            }
+        },
         Details : async function (event, row) {
             // console.log('Details', idx);
             if (row['details']['start_date'] === null) {
@@ -53,7 +58,7 @@ let contract = new Vue({
                             contract_id : row['outline']['contract_id']
                         })
                     }).then((res) => {return res.json();});
-                    console.log(result);
+                    // console.log(result);
                     if (result.status === 'ok') {
                         row['details']['start_date'] = result['data'][0]['start_date'];
                         row['details']['end_date'] = result['data'][0]['end_date'];
@@ -67,14 +72,16 @@ let contract = new Vue({
                 }
             }
             if (event.target.tagName === 'TD') {
-                row.delDisplay = !row.delDisplay;
+                row['display']['details'] = !row['display']['details'];
             }
-            console.log(this.rows);
         },
-        MarkContract : function (event, idx) {
+        MarkTermiContract : function (event, idx) {
             // console.log('DeleteContract', row);
-            event.target.blur();
-            this.toDelete.targetidx = idx;
+            if (this.toTerminate['targetidx'] === null) {
+                event.target.blur();
+                this.toTerminate['targetidx'] = idx;
+                this.toTerminate['targetcontract_id'] = this.rows[idx]['outline']['contract_id'];
+            }
         },
         SortTable : function (event, field) {
             if (this.rows.length > 1) {
@@ -90,14 +97,59 @@ let contract = new Vue({
                     this.lastSort = field;
                 }
             }
+        },
+        RefreshFilter : function (event) {
+            event.target.blur();
+            this.Filter['contract_id'] = this.Filter['publish_id'] = this.Filter['rent_id'] = '';
+            this.Filter['c_status'] = 'none';
+            this.rows.forEach((element) => {
+                element['display']['unit'] = true;
+            });
+        },
+        StartFilter : function () {
+            this.rows.forEach((row) => {
+                row['display']['unit'] = true;
+                this.thFields.forEach((field) => {
+                    if (this.Filter[field.name] !== '' && this.Filter[field.name] !== 'none') {
+                        let FilterValue = '';
+                        switch(field.name) {
+                            case'contract_id':
+                            case'publish_id':
+                            case'rent_id':
+                                FilterValue = parseInt(this.Filter[field.name], 10);
+                                break;
+                            case'c_status':
+                                FilterValue = this.Filter[field.name];
+                        }
+                        if (row['outline'][field.name] !== FilterValue) {
+                            row['display']['unit'] = false;
+                        }
+                    }
+                });
+                console.log(row['display']['unit']);
+            });
         }
     },
     watch : {
-        "toDelete.action" : function () {
-            if (this.toDelete.action) {
-                this.rows[this.toDelete.targetidx]['outline']['c_status'] = 'termination';
+        "toTerminate.action" : function (newValue, oldValue) {
+            if (this.toTerminate['action']) {
+                this.rows[this.toTerminate['targetidx']]['outline']['c_status'] = 'termination';
+                /*rewrite DB*/
             }
-            this.toDelete.targetidx = this.toDelete.action = null;
+            this.toTerminate['targetidx'] = this.toTerminate['action'] = this.toTerminate['targetcontract_id'] = null;
+            this.StartFilter();
+        },
+        Filter : {
+            handler (newValue, oldValue) {
+                console.log(this.Filter);
+                this.StartFilter();
+            },
+            deep:true
+        }
+    },
+    computed : {
+        NumOfField : function () {
+            return this.thFields.length;
         }
     }
 });
