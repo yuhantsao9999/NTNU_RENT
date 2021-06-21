f4e74829c6b35d39d225c87652ae93d24e24d125
yuhantsao9999 <yuhantsao9999@gmail.com>
2021-06-21 14:03:30 +0800

    feat:拿掉 sell 相關功能

--- controller/account.js
+++ controller/account.js
@@ -1,39 +1,37 @@
-const mysql = require('../model/db')
+const mysql = require('../model/db');
 
 const signIn = async (account) => {
-  const { email, password } = account
-  const sql = 'SELECT * FROM member WHERE email = ? and password = ?;'
-  const result = await mysql.query(sql, [email, password])
-    .catch((err) => {
-      console.log(err)
-    })
-  if (result.length > 0) {
-    const data = {
-      last_name: result[0].last_name,
-      first_name: result[0].first_name,
-      email: result[0].email,
+    const { email, password } = account;
+    const sql = 'SELECT * FROM member WHERE email = ? and password = ?;';
+    const result = await mysql.query(sql, [email, password]).catch((err) => {
+        console.log(err);
+    });
+    if (result.length > 0) {
+        const data = {
+            last_name: result[0].last_name,
+            first_name: result[0].first_name,
+            email: result[0].email,
+        };
+        return { error: false, data };
     }
-    return { error: false, data }
-  }
-  return { error: true }
-}
+    return { error: true };
+};
 
 const signUp = async (account) => {
-  const sql = 'INSERT INTO member SET ?'
-  const result = await mysql.query(sql, account)
-    .catch((err) => {
-      console.log(err)
-      return false
-    })
-  if (result) {
-    const data = {
-      last_name: account.last_name,
-      first_name: account.first_name,
-      email: account.email,
+    const sql = 'INSERT INTO member SET ?';
+    const result = await mysql.query(sql, account).catch((err) => {
+        console.log(err);
+        return false;
+    });
+    if (result) {
+        const data = {
+            last_name: account.last_name,
+            first_name: account.first_name,
+            email: account.email,
+        };
+        return { error: false, data };
     }
-    return { error: false, data }
-  }
-  return { error: true }
-}
+    return { error: true };
+};
 
-module.exports = { signIn, signUp }
+module.exports = { signIn, signUp };
--- routes/upload.js
+++ routes/upload.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { multerS3 } = require('../utils/multerS3');
 const { rentList } = require('../controller/rentList');
-const { sellList } = require('../controller/sellList');
 const imageCheck = require('../middlewares/imageCheck');
 const rentCheck = require('../middlewares/rentCheck');
 
@@ -13,19 +12,6 @@ router.post('/rent_upload', [
     async (req, res) => {
         console.log('enter /rent_upload');
         const result = await rentList(req.files, req.body.data);
-        if (result.error) {
-            res.status(404).send('Error on mysql.');
-        }
-        res.send(result.data);
-    },
-]);
-
-router.post('/sell_upload', [
-    multerS3.array('image'),
-    imageCheck,
-    rentCheck,
-    async (req, res) => {
-        const result = await sellList(req.files, req.body.data);
         if (result.error) {
             res.status(404).send('Error on mysql.');
         }
