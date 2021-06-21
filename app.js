468f332d05f9720adb3bbf3dab438a48697b8eee
yuhantsao9999 <yuhantsao9999@gmail.com>
2021-06-21 13:49:17 +0800

    feat:去除掉url 上的.html

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const app = express();
 app.use(bodyParser.json({ limit: '50000mb' }));
 app.use(bodyParser.urlencoded({ limit: '50000mb', extended: true }));
 
-app.use(express.static(path.join(__dirname, '/public'), { index: 'index.html' }));
+app.use(express.static(path.join(__dirname, '/public'), { index: 'index' }));
 app.use(express.static(__dirname, { dotfiles: 'allow' }));
+app.use(express.static('public', { extensions: ['html'] }));
 
 //const image = require('./routes/image');
 //app.use('/', image);
@@ -22,6 +23,5 @@ app.use('/admin', admin);
 app.get('/', (req, res) => {
     res.send('Very ok');
 });
-
 
 app.listen(3000, () => console.log('伺服器已經啟動在http://localhost:3000/'));
