e7589527cf4a572da5d94e546dc44b15a529f0a0
yuhantsao9999 <yuhantsao9999@gmail.com>
2021-06-22 10:49:13 +0800

    fix:解決衝突

--- controller/account.js
+++ controller/account.js
@@ -1,42 +1,6 @@
 const mysql = require('../model/db');
 
 const signIn = async (account) => {
-<<<<<<< HEAD
-    const { email, password } = account;
-    const sql = 'SELECT * FROM member WHERE email = ? and password = ?;';
-    const result = await mysql.query(sql, [email, password]).catch((err) => {
-        console.log(err);
-    });
-    if (result.length > 0) {
-        const data = {
-            last_name: result[0].last_name,
-            first_name: result[0].first_name,
-            email: result[0].email,
-        };
-        return { error: false, data };
-    }
-    return { error: true };
-};
-
-const signUp = async (account) => {
-    const sql = 'INSERT INTO member SET ?';
-    const result = await mysql.query(sql, account).catch((err) => {
-        console.log(err);
-        return false;
-    });
-    if (result) {
-        const data = {
-            last_name: account.last_name,
-            first_name: account.first_name,
-            email: account.email,
-        };
-        return { error: false, data };
-    }
-    return { error: true };
-};
-
-module.exports = { signIn, signUp };
-=======
 	try {
 		const {email, password} = account;
   		const sql = 'SELECT * FROM Users WHERE email = ? and password = ?';
@@ -64,4 +28,3 @@ const signUp = async (account) => {
 	}
 }
 module.exports = {signIn, signUp}
->>>>>>> c94388b06b458fb2b8cff4acdcd1e4252e8bd13f
