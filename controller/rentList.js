1b0da0a68c66b05c998e1d7d78ba714007dd36c3
yuhantsao9999 <yuhantsao9999@gmail.com>
2021-06-22 19:16:56 +0800

    fix:add some console

--- controller/rentList.js
+++ controller/rentList.js
@@ -3,20 +3,27 @@ const { findPath } = require('../utils/image');
 
 const rentList = async (files, data) => {
     const paths = findPath(files);
-    const re = /"/gi
-    const set = Array.from(data.substring(1, data.length - 1).replace(re, '').split(',')).concat([paths.toString()]);
+    const re = /"/gi;
+    const set = Array.from(
+        data
+            .substring(1, data.length - 1)
+            .replace(re, '')
+            .split(',')
+    ).concat([paths.toString()]);
     console.log(set);
     const user_id = await mysql.query('SELECT user_id FROM Users WHERE email = ?', set[0]).catch((err) => {
         console.log(err);
         return false;
-    })
+    });
+    console.log('rentList user_id', user_id);
 
     set.splice(0, 1);
     set.unshift(user_id[0].user_id);
 
-    const sql = 'INSERT INTO Product(user_id, p_name, category, brand, price, days, intro, place, photo) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    const sql =
+        'INSERT INTO Product(user_id, p_name, category, brand, price, days, intro, place, photo) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)';
     const result = await mysql.query(sql, set).catch((err) => {
-        console.log(err);
+        console.log('rentList err', err);
         return false;
     });
     if (result) {
