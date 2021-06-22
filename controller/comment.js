8d33fb710b0ff24b0f015e7ea32253d12f315a99
yuhantsao9999 <yuhantsao9999@gmail.com>
2021-06-22 10:50:32 +0800

    feat:建立評價系統

---
+++ controller/comment.js
@@ -0,0 +1,27 @@
+const mysql = require('../model/db');
+
+const comment_publish = async (data) => {
+    const { contract_id, star_number, comment_textarea } = data;
+    const sql = `INSERT Eval (contract_id, publish_star, publish_comment) VALUES (${contract_id},${star_number},${comment_textarea})`;
+    const result = await mysql.query(sql).catch((err) => {
+        return false;
+    });
+    if (result) {
+        return { error: false };
+    }
+    return { error: true };
+};
+
+const comment_rent = async (data) => {
+    const { contract_id, star_number, comment_textarea } = data;
+    const sql = `INSERT Eval (contract_id, rent_star, rent_comment) VALUES (${contract_id},${star_number},${comment_textarea})`;
+    const result = await mysql.query(sql).catch((err) => {
+        return false;
+    });
+    if (result) {
+        return { error: false };
+    }
+    return { error: true };
+};
+
+module.exports = { comment_publish, comment_rent };
